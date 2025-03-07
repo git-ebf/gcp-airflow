5a5f7e4321c27b9fff9e1eaf7118ebad6fc458ec
EBF <gitebf@gmail.com>
2025-03-07 00:58:35 -0300

    .

---
+++ dags/exemplo_dag.py
@@ -0,0 +1,13 @@
+
+│   from airflow import DAG
+│   from airflow.operators.python import PythonOperator
+│   from datetime import datetime
+│
+│   def tarefa_exemplo():
+│       print("Execução de exemplo.")
+│
+│   with DAG('exemplo_dag', start_date=datetime(2023, 1, 1), schedule_interval='@daily') as dag:
+│       exemplo = PythonOperator(
+│           task_id='exemplo_task',
+│           python_callable=tarefa_exemplo
+│       )
